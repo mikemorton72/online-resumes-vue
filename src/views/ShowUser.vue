4365cf380a5b869c9a8bf8eddefd73ac1f77a21e
Mike Morton <mmorton7295@gmail.com>
2021-08-03 19:55:42 -0500

    further builds upon student show page

--- src/views/ShowUser.vue
+++ src/views/ShowUser.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>User Show</h1>
-    
+
     <div class="card" style="width: 35rem;">
       <div class="card-body">
         <h5 class="card-title">Experiences</h5>
@@ -9,6 +9,70 @@
         <p class="card-text">All of my responsibilites at Company Name</p>
         <h6 class="card-subtitle mb-2 text-muted">Another Company Name</h6>
         <p class="card-text">All of my responsibilites at Another Company Name</p>
+      </div>
+    </div>
+    <br />
+    <div class="card" style="width: 35rem;">
+      <div class="card-body">
+        <h5 class="card-title">Education</h5>
+        <h6 class="card-subtitle mb-2 text-muted">School #1</h6>
+        <p class="card-text">All of my responsibilites at School Name</p>
+        <h6 class="card-subtitle mb-2 text-muted">Another School Name</h6>
+        <p class="card-text">All of my responsibilites at Another School Name</p>
+      </div>
+    </div>
+    <br />
+    <div class="card" style="width: 35rem;">
+      <div class="card-body">
+        <h5 class="card-title">Skills</h5>
+        <p class="card-text">Skill 1, skill 2, skill 3, skill 5</p>
+      </div>
+    </div>
+    <br />
+    <div class="card" style="width: 35rem;">
+      <div class="card-body">
+        <h5 class="card-title">Capstone</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Description</h6>
+        <p class="card-text">Capstone description text</p>
+        <p class="card-text">Capstone URL</p>
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://i.stack.imgur.com/y9DpT.jpg" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.stack.imgur.com/y9DpT.jpg" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://i.stack.imgur.com/y9DpT.jpg" class="d-block w-100" alt="">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
       </div>
     </div>
   </div>
