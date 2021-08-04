a698c475d02195ff46b51ee3832a815caa5f24af
Mike Morton <mmorton7295@gmail.com>
2021-08-03 19:34:55 -0500

    makes basic Student card for the index page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
+    <div class="container">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> -->
     </div>
     <router-view/>
+    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    <h1>User Index</h1>
+    <h1>All Students</h1>
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://s.yimg.com/uu/api/res/1.2/j.IuW7t1ZkqNb32aj9xaDw--~B/aD00MDA7dz02MDA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/Benzinga/fec49aa7467e3735885162a33b8e83d9" class="img-fluid rounded-start" alt="">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Student #1</h5>
+        <p class="card-text">Short Bio about student #1 who has learned many things.</p>
+        <p class="card-text"><a href="https://www.linkedin.com/in/mike-morton72/">LinkedIn</a></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
