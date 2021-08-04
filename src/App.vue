05f4cc58852c3d912db3bb272c8ff2146d0d9a75
Patrick <pdn6991@gmail.com>
2021-08-04 19:24:07 -0400

    adds confirmed style changes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="app">
     <div class="head bg-white text-light" align="center">
-      <a href="/"><img src="https://i.imgur.com/MCvfxZJ.jpg" style="width:600px;height:100px;"></a>
+      <a href="/"><img src="https://i.imgur.com/HkIMQrH.jpg" style="width:600px;height:155px;"></a>
       </div>
-      <!-- <a href="/"><img src="https://i.imgur.com/HkIMQrH.jpg" style="width:600px;height:155px;"></a>
-      </div> -->
-    <!-- <div class="container"> -->
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">Home</a>
-          <!-- <a class="navbar-brand" href="/"><img src="https://i.imgur.com/MCvfxZJ.jpg" style="width:600px;height:100px;"></a> -->
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -27,7 +23,6 @@
         </div>
       </nav>
     <router-view/>
-    <!-- </div> -->
   </div>
 </template>
 
