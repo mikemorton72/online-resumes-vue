bc64fe64388ec9afc4e4c8d80fbade1ad5cf3ce4
Patrick <pdn6991@gmail.com>
2021-08-04 13:58:27 -0400

    more button tweaks

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
       <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">Home</a>
+          <!-- <a class="navbar-brand" href="/"><img src="https://i.imgur.com/MCvfxZJ.jpg" style="width:600px;height:100px;"></a> -->
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
