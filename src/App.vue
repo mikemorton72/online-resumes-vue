0ec4ff5d18ed3e923373493fdd77beac9cf1eaf5
Mike Morton <mmorton7295@gmail.com>
2021-08-03 19:44:29 -0500

    creates basic user show page with experiences

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div class="container">
     <div id="nav">
-      <!-- <router-link to="/">Home</router-link> -->
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/users/2">User 2</router-link>
     </div>
     <router-view/>
     </div>
---
+++ src/views/ShowUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>User Show</h1>
+    
+    <div class="card" style="width: 35rem;">
+      <div class="card-body">
+        <h5 class="card-title">Experiences</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Company Name</h6>
+        <p class="card-text">All of my responsibilites at Company Name</p>
+        <h6 class="card-subtitle mb-2 text-muted">Another Company Name</h6>
+        <p class="card-text">All of my responsibilites at Another Company Name</p>
+      </div>
+    </div>
+  </div>
+</template>
